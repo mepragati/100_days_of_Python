575cc4bc6f7b8b8a9501c5e38a3222912198470f
Pragati Kumari <pragatikumari2122@gmail.com>
2021-08-16 23:39:56 +0530

    Changed folder name

---
+++ Day 001/BandNameGenerator.py
@@ -0,0 +1,5 @@
+#Band Name Generator Program
+print("Welcome to the Band Name Generator !!")
+name=input("What is the name of the city you grew up in?\n")
+pet=input("What is the name of the first pet you owned?\n")
+print("The name of your Band could be: "+name+" "+pet)
