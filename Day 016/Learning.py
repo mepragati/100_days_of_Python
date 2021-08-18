d3904095b94b07ad48571f3e2e5feac03432cd22
Pragati Kumari <pragatikumari2122@gmail.com>
2021-08-18 23:50:44 +0530

    Day 16 Learning Done

--- Day 016/Learning.py
+++ Day 016/Learning.py
@@ -1,14 +1,19 @@
-from turtle import Turtle, Screen
-tinny = Turtle()
-print(tinny)
-tinny.shape("turtle")
-tinny.color("coral")
-tinny.forward(100)
+# from turtle import Turtle, Screen
+# tinny = Turtle()
+# print(tinny)
+# tinny.shape("turtle")
+# tinny.color("coral")
+# tinny.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
 from prettytable import PrettyTable
 
 table = PrettyTable()
+
+table.add_column("Pokemon Name",["Pikachu", "Squirtle","Charmander"])
+table.add_column("Type",["Electric", "Water" , "Fire"])
+table.align="l"
+print(table)
