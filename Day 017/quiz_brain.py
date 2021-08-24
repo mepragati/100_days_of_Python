f672e9ecdfe359a3b0929895972c0c58dad5d932
Pragati Kumari <pragatikumari2122@gmail.com>
2021-08-24 23:54:01 +0530

    Day 17 QuizBrain

--- Day 017/quiz_brain.py
+++ Day 017/quiz_brain.py
@@ -1,5 +1,5 @@
 class QuizBrain:
-    
+    score =0 
     def __init__(self,q_list):
         self.question_number = 0
         self.question_list = q_list
@@ -7,7 +7,13 @@ class QuizBrain:
     def still_has_question(self):
         return self.question_number < len(self.question_list)
 
+    def is_correct(self,user_answer,answer):
+        pass
+    
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number+=1
-        input(f"Q.{self.question_number }: {current_question.text} (True/False)? : ")
+        answer = input(f"Q.{self.question_number }: {current_question.text} (True/False)? : ")
+
+    
+
