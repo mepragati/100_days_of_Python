7e02b7abb8d2ee9ac049da21718c7a9fd6147e1c
Pragati Kumari <pragatikumari2122@gmail.com>
2021-09-19 01:18:23 +0530

    Day 30 Upgraded Password Generator by adding searching option

--- Day 029-030/password-manager/main.py
+++ Day 029-030/password-manager/main.py
@@ -42,21 +42,43 @@ def save():
         is_ok = messagebox.askokcancel(title=website_entered , message = f"These are the details entered: \n Email: {email_entered}\n Password: {password_entered} \n Is it ok to save?")
 
         if is_ok:
-            with open("Day 029-030\password-manager\data.json", "r") as file:
-                #Reading Data
-                data = json.load(file)
+            try:
+                with open("Day 029-030\password-manager\data.json", "r") as file:
+                    #Reading old Data
+                    data = json.load(file)
+            except FileNotFoundError:
+                with open("Day 029-030\password-manager\data.json", "w") as file:
+                    json.dump(new_data,file,indent=4)
+            else:
                 #Updating the data
                 data.update(new_data)
 
-            with open("Day 029-030\password-manager\data.json", "w") as file:
-                #Saving updated data
-                json.dump(data,file,indent=4)
-
+                with open("Day 029-030\password-manager\data.json", "w") as file:
+                    #Saving updated data
+                    json.dump(data,file,indent=4)
+            finally:
                 input_password.delete(0,END)
                 input_website.delete(0,END)
 
 
- 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website_entered = input_website.get().title()
+    try:
+        with open("Day 029-030\password-manager\data.json") as file:
+            #Reading old Data
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message = "No Data File Found.")
+    else:
+        if website_entered in data:
+            email_found = data[website_entered]["email"]
+            password_found = data[website_entered]["password"]
+            messagebox.showinfo(title=f"{website_entered}", message = f" Email: {email_found}\n Password: {password_found}")
+        else:
+            messagebox.showinfo(title="Error", message = f"No details for {website_entered} exists.")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -79,11 +101,11 @@ label_password = Label(text="Password:")
 label_password.grid(column=0,row=3)
 
 #Entry
-input_website = Entry(width=35)
-input_website.grid(column=1,row=1,columnspan=2)
+input_website = Entry(width=21)
+input_website.grid(column=1,row=1)
 
 
-input_email = Entry(width=35)
+input_email = Entry(width=39)
 input_email.grid(column=1,row=2,columnspan=2)
 input_email.insert(0,"pragati@gmail.com")
 
@@ -98,6 +120,9 @@ button_generate.grid(column=2,row=3)
 
 button_add = Button(text="Add",width=36,command=save)
 button_add.grid(column=1,row=4,columnspan=2)
+
+button_search = Button(text="Search",width=14,command=find_password)
+button_search.grid(column=2,row=1)
 
 
 window.mainloop()
